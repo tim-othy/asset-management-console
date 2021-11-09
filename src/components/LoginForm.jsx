2a0637082bbc2b082f66fbd6bfc4733d2a002eed
tim-othy <timothy.mehay@gmail.com>
2021-11-09 20:49:17 +0100

    Add AssetView graph with placeholder data

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,20 +4,20 @@ import {SignInButton} from "./SignInButton";
 
 const LoginForm = () => (
     <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 450, maxHeight: 600 }}>
-            <Header
-                as='h1'
-                textAlign='center'
-            >
-                <Icon name='money bill alternate outline' color='grey' />
-            </Header>
-            <Header
-                as='h1'
-                color='teal'
-                textAlign='center'
-                content='Asset management console'
-            />
+        <Grid.Column style={{ maxWidth: 500 }}>
             <Segment padded='very'>
+                <Header
+                    as='h1'
+                    textAlign='center'
+                >
+                    <Icon name='money bill alternate outline' color='grey' />
+                </Header>
+                <Header
+                    as='h1'
+                    color='teal'
+                    textAlign='center'
+                    content='Asset management console'
+                />
                 <SignInButton />
             </Segment>
         </Grid.Column>

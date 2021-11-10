0998ac8dad107cea815925f30159d9663dd8dbfe
tim-othy <timothy.mehay@gmail.com>
2021-11-10 19:55:22 +0100

    Remove axios, update actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,31 +1,18 @@
-export const setTargetAsset = (targetAsset) => {
-    console.log('targetAsset')
-    console.log(targetAsset)
-    return {
+export const setTargetAsset = (targetAsset) => ({
         type: "SET_TARGET_ASSET",
         targetAsset
-    }
-}
+})
 
-export const fetchAssetDataRequested = (targetAsset) => {
-    console.log("called fetchAssetDataRequested")
-    return {
-        type: "FETCH_ASSET_DATA_REQUESTED",
-        targetAsset
-    }
-}
+export const fetchAssetDataRequested = (targetAsset) => ({
+    type: "FETCH_ASSET_DATA_REQUESTED",
+    targetAsset
+})
 
-export const fetchAssetDataSucceeded = (assetData) => {
-    console.log("called fetchAssetDataSucceeded")
-    return {
-        type: "FETCH_ASSET_DATA_SUCCEEDED",
-        assetData
-    }
-}
+export const fetchAssetDataSucceeded = (assetData) => ({
+    type: "FETCH_ASSET_DATA_SUCCEEDED",
+    assetData
+})
 
-export const fetchAssetDataFailed = () => {
-    console.log("called fetchAssetDataFailed")
-    return {
-        type: "FETCH_ASSET_DATA_FAILED"
-    }
-}
+export const fetchAssetDataFailed = () => ({
+    type: "FETCH_ASSET_DATA_FAILED"
+})
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -4,22 +4,17 @@ import {
     takeLatest
 } from 'redux-saga/effects'
 
-import axios from 'axios'
-
 import {
     fetchAssetDataSucceeded,
     fetchAssetDataFailed
 } from '../actions';
 
 function* fetchAssetDataSaga() {
-    console.log("called fetchAssetDataSaga")
     yield takeLatest("FETCH_ASSET_DATA_REQUESTED", fetchAssetData);
 }
 
 function* fetchAssetData(action) {
-    console.log("called fetchAssetData")
     try {
-        // const response = yield call(axios.get, 'http://localhost:5000/timeseries/AE')
         const response = yield call(
             fetchAlphaVantageExchangeRate,
             action.targetAsset
@@ -27,7 +22,6 @@ function* fetchAssetData(action) {
         console.log('resopnse', response.items.timeseries)
         yield put({ type: "FETCH_ASSET_DATA_SUCCEEDED", data: response.items.timeseries})
     } catch (e) {
-        console.log("failed")
         yield put(fetchAssetDataFailed())
     }
 }
